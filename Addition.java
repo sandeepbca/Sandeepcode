053faf78caff9e87c9301e6fbb106c4ed2eef822
Sandeep <sandeep828480@gmail.com>
2023-04-19 08:47:05 +0530

    initial commit

---
+++ Addition.java
@@ -0,0 +1,8 @@
+public class Addition {
+public static void main(String[] args) {
+   int  a=1;
+   int  d= 2;
+   int  c=a+d;
+    System.out.println(c);
+} 
+}
---
+++ Sum.java
@@ -0,0 +1,8 @@
+public class Sum {
+   public static void main(String[] args) {
+    int i = 10;
+    int j = 12;
+    int total = i+j;
+    System.out.println(total);
+   } 
+}
